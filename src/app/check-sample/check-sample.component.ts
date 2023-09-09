0a044828a3f8d264aaa0126568ab433bd55d4f1f
IagoOliveira23 <iago.ishift@gmail.com>
2023-09-08 21:45:20 -0300

    A ordem doCheck, Content e View

--- src/app/check-sample/check-sample.component.ts
+++ src/app/check-sample/check-sample.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit, DoCheck, AfterContentChecked, AfterViewChecked, AfterViewInit } from '@angular/core';
+import { Component, OnInit, AfterContentInit, DoCheck, AfterContentChecked, AfterViewChecked, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-check-sample',
   templateUrl: './check-sample.component.html',
   styleUrls: ['./check-sample.component.css']
 })
-export class CheckSampleComponent implements OnInit, DoCheck, AfterContentChecked, AfterViewChecked, AfterViewInit{
+export class CheckSampleComponent implements OnInit, DoCheck,AfterContentInit, AfterContentChecked, AfterViewChecked, AfterViewInit{
 
   quantidade:number = 0
 
   constructor(){}
+
+ 
   
   adicionar(){
     this.quantidade += 1
@@ -19,16 +21,28 @@ export class CheckSampleComponent implements OnInit, DoCheck, AfterContentChecke
     this.quantidade -= 1
   }
 
+  //checked -> content -> view
+
+   //quando o primeiro conteudo é iniciado
+   ngAfterContentInit(): void {
+    console.log("AfterContentInit");
+  }
+  
+  //depois da inicialização da view
+  ngAfterViewInit(): void {
+    console.log("AfterViewInit");
+    
+  }
+
+  //após alguma alteração, verifica o conteúdo
   ngAfterContentChecked(): void {
     console.log("AfterContentCheked");
     
   }
+
+  //após alguma alteração, verifica a view
   ngAfterViewChecked(): void {
     console.log("AfterViewChecked");
-    
-  }
-  ngAfterViewInit(): void {
-    console.log("AfterViewInit");
     
   }
   
